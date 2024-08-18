c0c1b82bf4f2e75c5f47c2ed013b9a45cdf66fe2
Vladislav Churilov <Nawaharjan@gmail.com>
2024-08-18 21:52:28 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 2 };
+            Console.WriteLine(SquareSum(numbers));
+        }
+        public static int SquareSum(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i] * numbers[i];
+            }
+            return sum;
+        }
+    }
+}
